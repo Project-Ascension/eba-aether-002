2674980c5212733314a4b4dbd1b4af330d6fb935
benwurth <ben.wurth@gmail.com>
2015-01-06 01:20:05 -0800

    Fixed Respawning Issue

---
+++ Assets/eba_damageHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eba_damageHandler : vp_DamageHandler {
+
+	private StatsScript _statsScript;
+	private MobBoss _mobBoss;
+	private GenericMob _thisMob;
+	protected vp_Timer.Handle m_DestroyTimer = new vp_Timer.Handle();
+
+	// Use this for initialization
+	void Start () {
+		_statsScript = GameObject.Find("StatsBot").GetComponent<StatsScript>();
+		_mobBoss = GameObject.Find("MobBoss").GetComponent<MobBoss>();
+		_thisMob = gameObject.GetComponent<GenericMob>();
+
+		if (!_thisMob)
+		{
+			Debug.LogError("No GenericMob Script on GameObject");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Overrides the Die() function from vp_DamageHandler
+	public virtual void Die()
+	{
+		_statsScript.AddKill();
+
+		base.Die();
+
+		_mobBoss.DeRegisterMob(_thisMob);
+
+//		Destroy(gameObject);
+	}
+}
